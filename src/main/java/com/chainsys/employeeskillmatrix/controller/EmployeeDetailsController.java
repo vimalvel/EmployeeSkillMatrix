4c41ec62731fcc572a323265a7a79e09ee9b1b4d
vimal <vimalvel.murugan@chainsys.com>
2022-08-17 15:26:16 +0530

    sonnarlint

--- src/main/java/com/chainsys/employeeskillmatrix/controller/EmployeeDetailsController.java
+++ src/main/java/com/chainsys/employeeskillmatrix/controller/EmployeeDetailsController.java
@@ -2,12 +2,10 @@ package com.chainsys.employeeskillmatrix.controller;
 
 import java.util.List;
 
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -54,7 +52,7 @@ public class EmployeeDetailsController {
 		return UPDATEEMPLOYEEFORM;
 	}
 	@PostMapping("/updatenewemployeedetails")
-	public String updateEmployeeDetails(@Valid@ModelAttribute("updateemployeedetails")EmployeeDetails employee , Errors errors) {
+	public String updateEmployeeDetails(@ModelAttribute("updateemployeedetails")EmployeeDetails employee) {
 		employeeDetailsService.save(employee);
 		return EMPLOYEELIST;
 	}
